db57667ad21b75fc745b79f58afa2e1fae1c9250
naufal-faiz <naufalfaizhidayat135@gmail.com>
2026-09-18 14:34:30 +0700

    perbaikan bug minor

--- app/(admin)/admin/training/jenis-alat/create/page.tsx
+++ app/(admin)/admin/training/jenis-alat/create/page.tsx
@@ -2,7 +2,7 @@ import { getAllTrainingCategories } from "@/lib/data/training/trainingCategory";
 import { HeavyEquipmentForm } from "@/components/admin/main/HeavyEquipment";
 
 const CreateHeavyEquipmentPage = async () => {
-    const categoriesResult = await getAllTrainingCategories({ take: 100 });
+    const categoriesResult = await getAllTrainingCategories({ limit: 100 });
 
     return (
         <HeavyEquipmentForm
